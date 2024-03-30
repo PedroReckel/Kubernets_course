54b493f823b782504c1b723bec23c80574248c7f
PedroReckel <pedroreckel90@gmail.com>
2024-03-30 16:54:43 -0300

    Trabalhando com Variáveis de Ambiente e ConfigMap.

--- aulas/server.go
+++ aulas/server.go
@@ -1,12 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+    "os"
+	"net/http"
+	"log"
+	"io/ioutil"
+)
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8001", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+
+	name := os.Getenv("NAME")
+	age  := os.Getenv("AGE")
+	
+	fmt.Fprint(w, "Hello, I'm %s. I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	
+	fmt.Fprint(w, "My Family: %s", string(data))
 }
