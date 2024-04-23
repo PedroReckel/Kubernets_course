8fe125e70d9b8e7d4e0f478106f63a98b160fd26
PedroReckel <pedroreckel90@gmail.com>
2024-04-22 21:37:14 -0300

    Entendendo Health Check, Liveness e Readiness

--- aulas/server.go
+++ aulas/server.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 	"log"
 	"io/ioutil"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/heathz", Heathz)
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
@@ -40,4 +44,19 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	fmt.Fprint(w, "My Family: %s", string(data))
+}
+
+// Garantir que a aplicação está saudavel
+func Heathz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+
 }
